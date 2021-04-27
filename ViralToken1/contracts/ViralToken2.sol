019376d2ace4e77345715b0ab4dc74de18ae491d
aeropriest <ashok@myhomi.io>
2021-04-28 06:27:38 +0800

    Added the token allocation alog

--- ViralToken1/contracts/ViralToken2.sol
+++ ViralToken1/contracts/ViralToken2.sol
@@ -1,7 +1,46 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >= 0.5.0 < 0.7.0;
+pragma solidity ^0.8.2;
 
 contract ViralToken2 {
-  constructor() public {
-  }
+    mapping(address => uint) public balances;
+    mapping(address => mapping(address => uint)) public allowence;
+    uint public totalSupply = 1000000;
+    string public name = "Viral Token 1";
+    string public symbol = "VRL1";
+    uint public decibals = 18; 
+    
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+    
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+    
+    function balanceOf(address owner) public view returns (uint){
+        return balances[owner];
+    }
+    
+    function transfer(address to, uint value) public returns (bool){
+        require(balanceOf(msg.sender) >= value, 'Not enough token to tansfer');
+        balances[to] += value;
+        balances[msg.sender] -= value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    function transferFrom(address from, address to, uint value) public returns (bool){
+        require(balanceOf(from) >= value, 'Not enough token to tansfer');
+        require(allowence[from][msg.sender] >= value, 'Not enough allowence to tansfer');
+        balances[to] += value;
+        balances[from] -= value;
+        emit Transfer(from, to, value);
+        return true;
+        
+    }
+    
+    function approve(address spender, uint value) public returns (bool) {
+        allowence[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+    
 }
